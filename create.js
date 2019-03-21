71daabae898551fa897ca3344fab339e610105c0
ElCateto <43753458+ElCateto@users.noreply.github.com>
2019-03-21 10:00:26 -0400

    magnet and apa

---
+++ create.js
@@ -0,0 +1,108 @@
+
+function brs(num) {
+  let returnStr = ' '
+  for (i = 0; i < num; i++){
+    returnStr = returnStr + '<br/>'
+  }
+  return returnStr
+}
+/////////////////////////////////////////////////////////////////////////////
+
+function ait() {
+  let content = document.getElementById('contentdiv')
+/////////////////////////////////////////////////////////////////////////////
+}
+function apa() {
+  location.href='apa.html'
+
+}
+function dance() {
+  location.href='dance.html'
+
+}
+function hello() {
+  location.href='hello.html'
+
+}
+function walkAway() {
+  location.href='walkaway.html'
+
+}
+function clas() {
+  location.href='class.html'
+
+}function sit() {
+  location.href='sit.html'
+
+}function newsit() {
+  location.href='newseat.html'
+
+}
+function apa1() {
+    text = "You bump into someone who is 'in character', he is starting to breathe heavy, what will you do?"
+    let content = document.getElementById('contentdiv')
+    let brStr = brs(5);
+    content.innerHTML = (brStr + text + brStr);
+}
+/////////////////////////////////////////////////////////////////////////////
+function robot() {
+  location.href='robot.html'
+
+}
+
+function auditorium() {
+  location.href='auditorium.html'
+
+}function tesla() {
+  location.href='tesla.html'
+
+}function magnet() {
+  location.href='magnet.html'
+
+}
+function magnet1() {
+    text = "You have been told to do one of three things, what will you do?"
+    let content = document.getElementById('contentdiv')
+    let brStr = brs(5);
+    content.innerHTML = (brStr + text + brStr);
+  }
+  function light(){
+    location.href='light.html'
+
+  }
+  function mclass(){
+    location.href='mclass.html'
+
+  }
+
+/////////////////////////////////////////////////////////////////////////////
+function uctech() {
+  text = 'UCTECH has a variety of vocations to choose from. Which one will you choose?'
+  let content = document.getElementById('contentdiv')
+  let brStr = brs(5);
+  content.innerHTML = (brStr + text + brStr);
+}
+function aahs() {
+  location.href='aahs.html'
+}
+function aahs1() {
+    text = 'You walk into Allied and find a student collapsed on the floor. What will you do?'
+    let content = document.getElementById('contentdiv')
+    let brStr = brs(5);
+    content.innerHTML = (brStr + text + brStr);
+}
+function ignore() {
+  location.href='ignore.html'
+}
+function returnTitle() {
+  location.href='./index.html'
+}
+
+function party(){
+  location.href='party.html'
+}
+
+function yatch(){
+  location.href='yatch.html'
+}
+/////////////////////////////////////////////////////////////////////////////
